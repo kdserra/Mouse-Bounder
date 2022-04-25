6cc450e3080504a6cc0a72e56d6ca8f1c85b39ff
Kevin Serra <kevindanielserra@gmail.com>
2022-04-25 18:03:12 -0400

    Create Utilities.cs

---
+++ Mouse-Bounder/Utilities.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using WindowsHook;
+
+namespace Mouse_Bounder
+{
+    public class Utilities
+    {
+        private static Rect m_BorderRect = new Rect(-7, 0, 7, 7);
+        
+        [StructLayout(LayoutKind.Sequential)]
+        private struct RECT
+        {
+            public int left;
+            public int top;
+            public int right;
+            public int bottom;
+        }
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        private static extern IntPtr FindWindow(string strClassName, string strWindowName);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern bool GetWindowRect(IntPtr hWnd, ref RECT Rect);
+
+        /// <summary>
+        /// Fetches window rect.
+        /// Returns null if the window is not found.
+        /// </summary>
+        public static Rect? GetWindowRect(Process process)
+        {
+            if (process == null) { return null; }
+            RECT rect = new RECT();
+            IntPtr hwnd = FindWindow(null, process.MainWindowTitle);
+            bool state = GetWindowRect(hwnd, ref rect);
+            if (!state) { return null; }
+            return new Rect(rect.left, rect.top, rect.right, rect.bottom);
+        }
+
+        /// <summary>
+        /// Fetches window rect without the invisible borders.
+        /// Returns null if the window is not found.
+        /// </summary>
+        public static Rect? GetAdjustedWindowRect(Process process)
+        {
+            if (process == null) { return null; }
+            RECT rect = new RECT();
+            IntPtr hwnd = FindWindow(null, process.MainWindowTitle);
+            bool state = GetWindowRect(hwnd, ref rect);
+            if (!state) { return null; }
+            return new Rect(rect.left   - m_BorderRect.Left,
+                            rect.top    - m_BorderRect.Top,
+                            rect.right  - m_BorderRect.Right,
+                            rect.bottom - m_BorderRect.Bottom);
+        }
+    }
+}
