4c42afbf9889296c5c5cf7d64bdda5acf4115067
Kevin Serra <kevindanielserra@gmail.com>
2022-04-25 18:03:04 -0400

    Update ProcessMouseBounder.cs

--- Mouse-Bounder/ProcessMouseBounder.cs
+++ Mouse-Bounder/ProcessMouseBounder.cs
@@ -1,49 +1,41 @@
-﻿using Gma.System.MouseKeyHook;
-using System;
-using System.Diagnostics;
+﻿using System.Diagnostics;
 using System.Drawing;
-using System.Runtime.InteropServices;
 using System.Windows.Forms;
+using WindowsHook;
 
 namespace Mouse_Bounder
 {
     public static class ProcessMouseBounder
     {
-        [StructLayout(LayoutKind.Sequential)]
-        public struct RECT
-        {
-            public int left;
-            public int top;
-            public int right;
-            public int bottom;
-        }
+        public delegate void BoundHandler();
+        public static event BoundHandler OnBound;
+        public delegate void UnboundHandler();
+        public static event UnboundHandler OnUnbound;
 
-        [DllImport("user32.dll", CharSet = CharSet.Auto)]
-        public static extern IntPtr FindWindow(string strClassName, string strWindowName);
-
-        [DllImport("user32.dll", SetLastError = true)]
-        static extern bool GetWindowRect(IntPtr hWnd, ref RECT Rect);
-
-        private static bool m_IsBound = false;
         private static IKeyboardMouseEvents m_GlobalHook;
+        private static Process m_BoundProcess;
         private static Rect m_BoundRect;
         private static bool m_IsSubscribed = false;
+        public static bool IsBound { get; private set; } = false;
 
-        public static void BoundProcess(Process process)
+        public static void Bound(Process process)
         {
             if (process == null) { return; }
-            if (m_IsBound) { ResetState(); }
-            Rect? boundRect = GetWindowRect(process);
+            m_BoundProcess = process;
+            if (IsBound) { ResetState(); }
+            Rect? boundRect = Utilities.GetAdjustedWindowRect(process);
             if (boundRect == null) { ResetState(); return; }
             Bound((Rect)boundRect);
         }
 
         public static void Bound(Rect rect)
         {
-            if (m_IsBound) { ResetState(); }
+            if (IsBound) { ResetState(); }
             m_BoundRect = rect;
             Subscribe();
-            m_IsBound = true;
+            IsBound = true;
+            Cursor.Position = BoundPosition(Cursor.Position, m_BoundRect);
+            OnBound?.Invoke();
         }
 
         public static void Unbound()
@@ -54,42 +46,44 @@ namespace Mouse_Bounder
         private static void ResetState()
         {
             if (m_IsSubscribed) { Unsubscribe(); }
+            m_BoundProcess = null;
+            IsBound = false;
+            OnUnbound?.Invoke();
         }
 
-        private static Rect? GetWindowRect(Process process)
-        {
-            if (process == null) { return null; }
-            RECT rect = new RECT();
-            IntPtr hwnd = FindWindow(null, process.MainWindowTitle);
-            bool state = GetWindowRect(hwnd, ref rect);
-            if (!state) { return null; }
-            return new Rect(rect.left, rect.top, rect.right, rect.bottom);
-        }
 
-        private static void GlobalHookMouseMove(object sender, MouseEventArgs e)
+
+        private static void GlobalHookMouseMoveExt(object sender, WindowsHook.MouseEventExtArgs e)
         {
-            if (!m_IsBound) { ResetState();  return; }
-            //Debug.WriteLine($"MouseMove: \t{e.Location}");
-            //Debug.WriteLine($"Is Within Rect: \t{ m_BoundRect.Contains(e.Location)}");
+            Rect? windowRect = Utilities.GetAdjustedWindowRect(m_BoundProcess);
+            if (windowRect == null) { ResetState(); return; }
+            m_BoundRect = (Rect)windowRect;
             if (m_BoundRect.Contains(e.Location)) { return; }
-            if (e.Location.X < m_BoundRect.Left)   { Cursor.Position = new Point(m_BoundRect.Left,  e.Location.Y);  }
-            if (e.Location.X > m_BoundRect.Right)  { Cursor.Position = new Point(m_BoundRect.Right, e.Location.Y);  }
-            if (e.Location.Y < m_BoundRect.Top)    { Cursor.Position = new Point(e.Location.X, m_BoundRect.Top);    }
-            if (e.Location.Y > m_BoundRect.Bottom) { Cursor.Position = new Point(e.Location.X, m_BoundRect.Bottom); }
-            Debug.WriteLine($"Position (Before): {e.Location}");
-            Debug.WriteLine($"Position (After): {Cursor.Position}");
+            Cursor.Position = BoundPosition(e.Location, m_BoundRect);
+            e.Handled = true;
         }
 
+        private static Point BoundPosition(Point position, Rect boundRect)
+        {
+            if (boundRect.Contains(position)) { return position; }
+            if (position.X < boundRect.Left)   { position = new Point(boundRect.Left, position.Y);   }
+            if (position.X > boundRect.Right)  { position = new Point(boundRect.Right, position.Y);  }
+            if (position.Y < boundRect.Top)    { position = new Point(position.X, boundRect.Top);    }
+            if (position.Y > boundRect.Bottom) { position = new Point(position.X, boundRect.Bottom); }
+            return position;
+
+        }
+        
         private static void Subscribe()
         {
             m_GlobalHook = Hook.GlobalEvents();
-            m_GlobalHook.MouseMove += GlobalHookMouseMove;
+            m_GlobalHook.MouseMoveExt += GlobalHookMouseMoveExt;
             m_IsSubscribed = true;
         }
 
         private static void Unsubscribe()
         {
-            m_GlobalHook.MouseMove -= GlobalHookMouseMove;
+            m_GlobalHook.MouseMoveExt += GlobalHookMouseMoveExt;
             m_GlobalHook.Dispose();
             m_IsSubscribed = false;
         }
