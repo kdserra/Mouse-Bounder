0f0fa7972125e028a3437445c49ca6a6a1b09121
Kevin Serra <kevindanielserra@gmail.com>
2022-04-25 22:31:14 -0400

    Update HelpForm.Designer.cs

--- Mouse-Bounder/HelpForm.Designer.cs
+++ Mouse-Bounder/HelpForm.Designer.cs
@@ -32,8 +32,6 @@
             this.label3 = new System.Windows.Forms.Label();
             this.versionLbl = new System.Windows.Forms.Label();
             this.titleLbl = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label3
@@ -67,32 +65,11 @@
             this.titleLbl.TabIndex = 9;
             this.titleLbl.Text = "Mouse Bounder";
             // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 246);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(365, 120);
-            this.label1.TabIndex = 12;
-            this.label1.Text = resources.GetString("label1.Text");
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label2.Location = new System.Drawing.Point(12, 210);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(130, 25);
-            this.label2.TabIndex = 11;
-            this.label2.Text = "Manual Mode";
-            // 
             // HelpForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(452, 383);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.label2);
+            this.ClientSize = new System.Drawing.Size(452, 201);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.versionLbl);
             this.Controls.Add(this.titleLbl);
@@ -112,7 +89,5 @@
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label versionLbl;
         private System.Windows.Forms.Label titleLbl;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
     }
 }
