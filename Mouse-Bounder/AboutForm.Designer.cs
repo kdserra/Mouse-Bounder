d0bcb8cec308a1f366856e1407b2d54c95150526
Kevin Serra <kevindanielserra@gmail.com>
2023-06-29 03:19:26 -0400

    Update AboutForm.Designer.cs

--- Mouse-Bounder/AboutForm.Designer.cs
+++ Mouse-Bounder/AboutForm.Designer.cs
@@ -29,70 +29,69 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AboutForm));
-            this.titleLbl = new System.Windows.Forms.Label();
-            this.versionLbl = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
-            this.SuspendLayout();
+            titleLbl = new System.Windows.Forms.Label();
+            versionLbl = new System.Windows.Forms.Label();
+            label3 = new System.Windows.Forms.Label();
+            linkLabel1 = new System.Windows.Forms.LinkLabel();
+            SuspendLayout();
             // 
             // titleLbl
             // 
-            this.titleLbl.AutoSize = true;
-            this.titleLbl.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.titleLbl.Location = new System.Drawing.Point(12, 9);
-            this.titleLbl.Name = "titleLbl";
-            this.titleLbl.Size = new System.Drawing.Size(204, 37);
-            this.titleLbl.TabIndex = 0;
-            this.titleLbl.Text = "Mouse Bounder";
+            titleLbl.AutoSize = true;
+            titleLbl.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            titleLbl.Location = new System.Drawing.Point(12, 9);
+            titleLbl.Name = "titleLbl";
+            titleLbl.Size = new System.Drawing.Size(204, 37);
+            titleLbl.TabIndex = 0;
+            titleLbl.Text = "Mouse Bounder";
             // 
             // versionLbl
             // 
-            this.versionLbl.AutoSize = true;
-            this.versionLbl.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.versionLbl.Location = new System.Drawing.Point(12, 57);
-            this.versionLbl.Name = "versionLbl";
-            this.versionLbl.Size = new System.Drawing.Size(118, 25);
-            this.versionLbl.TabIndex = 0;
-            this.versionLbl.Text = "Version 2.0.0";
+            versionLbl.AutoSize = true;
+            versionLbl.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            versionLbl.Location = new System.Drawing.Point(12, 57);
+            versionLbl.Name = "versionLbl";
+            versionLbl.Size = new System.Drawing.Size(118, 25);
+            versionLbl.TabIndex = 0;
+            versionLbl.Text = "Version 2.1.0";
             // 
             // label3
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(12, 93);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(89, 15);
-            this.label3.TabIndex = 5;
-            this.label3.Text = "Stay up to date:";
+            label3.AutoSize = true;
+            label3.Location = new System.Drawing.Point(12, 93);
+            label3.Name = "label3";
+            label3.Size = new System.Drawing.Size(89, 15);
+            label3.TabIndex = 5;
+            label3.Text = "Stay up to date:";
             // 
             // linkLabel1
             // 
-            this.linkLabel1.AutoSize = true;
-            this.linkLabel1.Location = new System.Drawing.Point(12, 108);
-            this.linkLabel1.Name = "linkLabel1";
-            this.linkLabel1.Size = new System.Drawing.Size(243, 15);
-            this.linkLabel1.TabIndex = 7;
-            this.linkLabel1.TabStop = true;
-            this.linkLabel1.Text = "https://github.com/kdserra/Mouse-Bounder";
-            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            linkLabel1.AutoSize = true;
+            linkLabel1.Location = new System.Drawing.Point(12, 108);
+            linkLabel1.Name = "linkLabel1";
+            linkLabel1.Size = new System.Drawing.Size(243, 15);
+            linkLabel1.TabIndex = 7;
+            linkLabel1.TabStop = true;
+            linkLabel1.Text = "https://github.com/kdserra/Mouse-Bounder";
+            linkLabel1.LinkClicked += linkLabel1_LinkClicked;
             // 
             // AboutForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(322, 148);
-            this.Controls.Add(this.linkLabel1);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.versionLbl);
-            this.Controls.Add(this.titleLbl);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
-            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.MaximizeBox = false;
-            this.MinimizeBox = false;
-            this.Name = "AboutForm";
-            this.Text = "Mouse Bounder [About]";
-            this.ResumeLayout(false);
-            this.PerformLayout();
-
+            AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            ClientSize = new System.Drawing.Size(322, 148);
+            Controls.Add(linkLabel1);
+            Controls.Add(label3);
+            Controls.Add(versionLbl);
+            Controls.Add(titleLbl);
+            FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            Icon = (System.Drawing.Icon)resources.GetObject("$this.Icon");
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Name = "AboutForm";
+            Text = "Mouse Bounder [About]";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
